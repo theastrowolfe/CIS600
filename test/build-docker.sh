d65df339a5d37f860416c358d8c8af518eb278cf
Joshua Wolfe <developer.wolfe@gmail.com>
2022-10-14 22:37:48 -0500

    finish containing python notebook env

--- test/build-docker.sh
+++ test/build-docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker rm -f CIS600
-docker build -t CIS600 . && \
-docker run --name=CIS600 --rm -p 8080:8080 -it CIS600
+set -e
+docker build -t cis600 . && \
+docker run -v $(pwd)/notebooks:/opt/notebooks --name cis600 --rm -p 8888:8888 -it cis600
